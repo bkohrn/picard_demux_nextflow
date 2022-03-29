8a1ef0c4f0057192de2c2cf4ac1fb90ec72c9a61
Brendan Kohrn <kohrnb@uw.edu>
2022-03-29 14:33:32 -0700

    Add ability to have run multiple lanes at the same time

--- templates/extract_barcodes.sh
+++ templates/extract_barcodes.sh
@@ -13,7 +13,7 @@ picard -Xmx${params.mem_amount}${params.mem_type} \\
     BASECALLS_DIR=\$PWD/${params.in_dir}/Data/Intensities/BaseCalls/ \\
     BARCODE_FILE=${barcode_file} \\
     READ_STRUCTURE=${params.read_struct} \\
-    LANE=${params.lane} \\
+    LANE=${lane} \\
     OUTPUT_DIR=\$OUTDIR  \\
     METRICS_FILE=${params.out_prefix}_barcode_metrics.txt
 } 2>&1 | tee -a extract_barcodes.log
---
+++ templates/merge_fastqs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x
+
+
+echo ${inName}
+echo ${inDir}
+ls
+
